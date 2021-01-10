9d3e05aa4fdace1a55dd0b7d5342e573778d4315
Dewa Dwi <51353195+dewade22@users.noreply.github.com>
2021-01-10 21:04:11 +0800

    Styling Halaman Create Blog

--- src/pages/CreateBlog/index.js
+++ src/pages/CreateBlog/index.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Button, Gap, Input, TextArea, Upload } from '../../components';
+import './createBlog.scss';
 
 const CreateBlog = () => {
     return (
-        <div>
-            <p>Content Create Blog</p>
+        <div className="blog-post">
+            <p className="title">Create Blog</p>
+            <Input label="Post Title"/>
+            <Upload />
+            <TextArea />
+            <Gap height={20} />
+            <div className="button-action">
+                <Button title="Save" />
+            </div>
         </div>
     )
 }
